24600e1dc3eab0c70b3e4280d363557b2e0b0c79
progeuler <sarufkhan568@gmail.com>
2025-05-11 17:45:38 +0600

    feat: add @vercel/analytics

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import Navbar from './components/NavBar'
 import Footer from './components/Footer'
 import { ToastProvider } from './components/ui/ToastProvider'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Root() {
   return (
     <div>
+        <Analytics />
         <Navbar />
         <ToastProvider>
             <Outlet />
